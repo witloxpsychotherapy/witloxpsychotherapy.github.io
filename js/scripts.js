78b690fd0ea21bcfa67453ceddfa060e7f262e90
Kevin Witlox <kevinwit@gmail.com>
2022-02-03 11:35:35 -0500

    fixed silly js error

--- js/scripts.js
+++ js/scripts.js
@@ -43,7 +43,7 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
     //if no touch capabilities detected, add hover effects
-    if !('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) {
-        document.body.querySelectorAll('.fillbox').forEach((el) => el.classList.add('dt'))
+    if (!('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0)) {
+        document.body.querySelectorAll('.fillbox').forEach((el) => el.classList.add('dt'));
     }
 });
